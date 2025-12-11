2311a92c5da9b729df7ccaa07a740595ef2c8a8e
Atul Nale <aryannale@gmail.com>
2025-12-11 11:36:28 +0530

    solved problem 4

--- src/arrays/mod.rs
+++ src/arrays/mod.rs
@@ -2,3 +2,4 @@ pub mod p1_first_missing_integer;
 pub mod p2_check_is_sorted_array;
 
 pub mod p3_rotate_array;
+pub mod p4_move_zeros;
---
+++ src/arrays/p4_move_zeros.rs
@@ -0,0 +1,25 @@
+//! Leetcode 283. Move Zeroes
+//!
+//! URL: https://leetcode.com/problems/move-zeroes/description/
+//!
+//! Submission: 100% runtime, 15.83% memory
+struct Solution{}
+impl Solution {
+    pub fn move_zeroes(nums: &mut Vec<i32>) {
+        let mut left = 0;
+        for right in 0..nums.len() {
+            if nums[right] != 0 {
+                let mut temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left += 1;
+            }
+        }
+    }
+}
+#[test]
+fn test1() {
+    let mut nums = vec![0,1,0,3,12];
+    Solution::move_zeroes(&mut nums);
+    println!("{:?}",nums);
+}
