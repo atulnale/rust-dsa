b27d5597c9bebb503d883ae5624575d0de5e221c
Atul Nale <aryannale@gmail.com>
2025-12-12 19:26:54 +0530

    updated code of problem7

--- src/linked_list/p7_add_two_numbers.rs
+++ src/linked_list/p7_add_two_numbers.rs
@@ -8,58 +8,25 @@ use crate::linked_list::list_node::ListNode;
 
 struct Solution {}
 impl Solution {
-    // pub fn add_two_numbers(
-    //     mut l1: Option<Box<ListNode>>,
-    //     mut l2: Option<Box<ListNode>>,
-    // ) -> Option<Box<ListNode>> {
-    //     let mut result = ListNode {
-    //         val: -1,
-    //         next: None,
-    //     };
-    //     let mut ptr = &mut result;
-    //     while l1.is_some() && l2.is_some() {
-    //         let node1 = l1.unwrap();
-    //         let node2 = l2.unwrap();
-    //         match node1.val.cmp(&node2.val) {
-    //             std::cmp::Ordering::Equal | std::cmp::Ordering::Less  => {
-    //                 ptr.next = l1;
-    //                 ptr = ptr.next.as_deref_mut().unwrap();
-    //                 l1 = ptr.next.take();
-    //             },
-    //             std::cmp::Ordering::Greater => {
-    //                 ptr.next = l2;
-    //                 ptr = ptr.next.as_deref_mut().unwrap();
-    //                 l2 = ptr.next.take();
-    //             }
-    //         }
-    //     }
-    //     if l1.is_none() {
-    //         ptr.next = l1;
-    //     }
-    //     if l2.is_none() {
-    //         ptr.next = l2;
-    //     }
-    //     result.next
-    // }
+    
     pub fn add_two_numbers(mut l1: Option<Box<ListNode>>, mut l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut carry = 0;
         let mut head = ListNode {
             val: -1,
             next: None
         };
         let mut ptr = &mut head;
-        let mut carry = 0;
         while l1.is_some() || l2.is_some() || carry > 0 {
-            let n1 = l1.as_ref().map_or(0, |node| node.val);
-            let n2 = l2.as_ref().map_or(0, |node| node.val);
-            let sum = n1 + n2 + carry;
-
+            let val1 = l1.as_ref().map_or(0, |node| node.val);
+            let val2 = l2.as_ref().map_or(0, |node| node.val);
+            let sum = val1 + val2 + carry;
             carry = sum / 10;
-            ptr.next = Some(Box::new (ListNode {
-                val: sum % 10,
-                next: None
-            }));
+            ptr.next = Some(
+                Box::new(
+                    ListNode { val: sum % 10, next: None }
+                )
+            );
             ptr = ptr.next.as_deref_mut().unwrap();
-
             if let Some(node) = l1 {
                 l1 = node.next;
             }
@@ -67,6 +34,7 @@ impl Solution {
                 l2 = node.next;
             }
         }
+
         head.next
     }
 
@@ -74,8 +42,8 @@ impl Solution {
 
 #[test]
 fn test() {
-    let head1 = ListNode::build(vec![1,2,3]);
-    let head2 = ListNode::build(vec![1,2,3,4]);
+    let head1 = ListNode::build(vec![9]);
+    let head2 = ListNode::build(vec![9]);
     let res = Solution::add_two_numbers(head1, head2);
     ListNode::display(&res);
 }
