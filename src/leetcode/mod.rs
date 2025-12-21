aef023028e3b92675803a3becaf0366c6683b8e6
Atul Nale <aryannale@gmail.com>
2025-12-21 10:19:09 +0530

    solved problem 4 of leetcode

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,2 +1,4 @@
 pub mod p00001;
 pub mod p00002;
+pub mod p00003;
+pub mod p00004;
---
+++ src/leetcode/p00003.rs
@@ -0,0 +1,43 @@
+
+struct Solution{}
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        let mut map: std::collections::HashMap<char, usize> = std::collections::HashMap::new();
+        let mut i:usize = 0;
+        let mut ans = 0;
+        for (j,ele) in s.chars().enumerate() {
+            if let Some(&x) = map.get(&ele) {
+                if x >= i {
+                    i = x + 1;
+                }
+            }
+            ans = ans.max(j - i + 1);
+            map.insert(ele, j);
+        }
+        return ans as i32;
+    }
+}
+
+#[test]
+fn test1() {
+    let s = "abba".to_string();
+    assert_eq!(2, Solution::length_of_longest_substring(s));
+}
+
+#[test]
+fn test2() {
+    let s = "abcabcbb".to_string();
+    assert_eq!(2, Solution::length_of_longest_substring(s));
+}
+
+#[test]
+fn test3() {
+    let s = "pwwkew".to_string();
+    assert_eq!(3, Solution::length_of_longest_substring(s));
+}
+
+#[test]
+fn test4() {
+    let s = "".to_string();
+    assert_eq!(0, Solution::length_of_longest_substring(s));
+}
