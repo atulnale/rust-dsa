2a042198f09c1c8fdddab71d148f08907d0484ab
Atul Nale <aryannale@gmail.com>
2025-12-24 22:41:48 +0530

    solved problem 10

--- src/leetcode/p00010.rs
+++ src/leetcode/p00010.rs
@@ -5,38 +5,14 @@ impl Solution {
     }
 
     pub fn is_regex_match(s: &Vec<char>, p: &Vec<char>, mut i: usize, mut j: usize) -> bool {
-        if i == s.len() && j == p.len() {
-            return true;
+        if j == p.len() {
+            return i == s.len();
         }
-        if i == s.len() {
-            if j + 1 < p.len() && p[j + 1] == '*' {
-                return Self::is_regex_match(s, p, i, j + 2);
-            } else {
-                return false;
-            }
+        let is_char_match = i != s.len() && (s[i] == p[j] || p[j] == '.');
+        if j + 1 < p.len() && p[j + 1] == '*' {
+            return Self::is_regex_match(s, p, i, j + 2) || (is_char_match && Self::is_regex_match(s, p, i + 1, j));
         }
-        if j >=p.len() {
-            return false;
-        }
-
-        if s[i] == p[j] || p[j] == '.' {
-            let mut res1 = false;
-            let mut has_star = false;
-            if j + 1 < p.len() && p[j + 1] == '*' {
-                res1 = Self::is_regex_match(s, p, i + 1, j) || Self::is_regex_match(s, p, i, j + 2);
-                has_star = true;
-            }
-            if has_star {
-                j = j + 2;
-            } else {
-                j = j + 1;
-            }
-
-            return res1 || Self::is_regex_match(s, p, i + 1, j);
-        } else if j + 1 < p.len() && p[j + 1] == '*' {
-            return Self::is_regex_match(s, p, i, j + 2);
-        }
-        return false;
+        return is_char_match && Self::is_regex_match(s, p, i + 1, j + 1);
     }
 }
 
@@ -111,7 +87,7 @@ mod tests {
     #[test]
     fn test9() {
         assert_eq!(
-            true,
+            false,
             Solution::is_match(String::from("aaaaaaaaaaaaaaaaaaab"), String::from("a*a*a*a*a*a*a*a*a*a*"))
         );
     }
